87b6f17d17c657a5f3a117dedecb36023cfa44ff
Evan Genest <genest.1@osu.edu>
2019-02-05 06:34:00 -0600

    Remove css vars.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,7 +1,7 @@
 let gulp = require('gulp');
 
 let autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
+//cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 postcss = require('gulp-postcss'),
 cssImport = require('postcss-import');
@@ -11,6 +11,6 @@ cssImport = require('postcss-import');
 
 gulp.task('styles', function() {
   console.log('Before piping css...');
-  return gulp.src('/Users/evan/Dropbox/code/portfolio-2019/src/styles/**/*.css').pipe(postcss([cssImport, cssvars, nested, autoprefixer])).pipe(gulp.dest('./build/styles/'));
+  return gulp.src('/Users/evan/Dropbox/code/portfolio-2019/src/styles/**/*.css').pipe(postcss([cssImport, nested, autoprefixer])).pipe(gulp.dest('./build/styles/'));
   console.log('...after piping css.');
 });
