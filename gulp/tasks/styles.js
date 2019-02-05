0a68cab9b60cac91d5e682253515f56c669f06e3
Evan Genest <genest.1@osu.edu>
2019-02-05 07:12:24 -0600

    Archived gulp file.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -11,6 +11,6 @@ cssImport = require('postcss-import');
 
 gulp.task('styles', function() {
   console.log('Before piping css...');
-  return gulp.src('/Users/evan/Dropbox/code/portfolio-2019/src/styles/**/*.css').pipe(postcss([cssImport, nested, autoprefixer])).pipe(gulp.dest('./build/styles/'));
+  return gulp.src('./src/styles/**/*.css').pipe(postcss([cssImport, nested, autoprefixer])).pipe(gulp.dest('./build/styles/'));
   console.log('...after piping css.');
 });
