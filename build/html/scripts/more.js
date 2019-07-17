00e388c1b7a894ecb6e75cde4bd58ba4658ba3e9
atom-box <genest.1@osu.edu>
2019-07-17 14:52:50 -0500

    Put links to clickable functions for MORE and LESS.

---
+++ build/html/scripts/more.js
@@ -0,0 +1,12 @@
+console.log(`portfolio javascript!`);
+
+function stretchVerbiage (s){
+	// @param The string to insert to the web page
+	// @return nothing
+	console.log(`This is ${this}`);
+
+}
+
+function tersifyVerbiage (s){
+	console.log(`This is ${this}`);
+}
