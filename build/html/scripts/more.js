ba361afa5ae107466e162ce175f87ad10f23f58d
atom-box <genest.1@osu.edu>
2019-07-17 16:07:13 -0500

    Make the H4 switch in one direction.

--- build/html/scripts/more.js
+++ build/html/scripts/more.js
@@ -4,9 +4,25 @@ function stretchVerbiage (s){
 	// @param The string to insert to the web page
 	// @return nothing
 	console.log(`This is ${this}`);
+	var elShort = document.getElementById('short001');
+	elShort.innerHTML = "Hey, I'm Zorak."
 
 }
 
 function tersifyVerbiage (s){
 	console.log(`This is ${this}`);
 }
+
+document.getElementById('swappable002').addEventListener('click', function(){
+	console.log(`Tomorrow never knows.`);
+	this.attic = this.innerHTML;
+	this.innerHTML = "Drive My Car";
+
+})
+
+if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
