069ccbd06dd723230c12c5bb887d68f93d3c8430
Evan Genest <genest.1@osu.edu>
2019-07-18 12:29:22 -0500

    Can swap to longer but not back to shorter.

--- build/html/scripts/more.js
+++ build/html/scripts/more.js
@@ -1,28 +1,32 @@
 console.log(`portfolio javascript!`);
 
-function stretchVerbiage (s){
-	// @param The string to insert to the web page
-	// @return nothing
-	console.log(`This is ${this}`);
-	var elShort = document.getElementById('short001');
-	elShort.innerHTML = "Hey, I'm Zorak."
-
-}
+// function stretchVerbiage (s){
+// 	// @param The string to insert to the web page
+// 	// @return nothing
+// 	console.log(`This is ${this}`);
+// 	var elShort = document.getElementById('short001');
+// 	elShort.innerHTML = "Hey, I'm Zorak."
+// }
 
 function tersifyVerbiage (s){
 	console.log(`This is ${this}`);
 }
 
-document.getElementById('swappable002').addEventListener('click', function(){
-	console.log(`Tomorrow never knows.`);
+var el001 = document.getElementById('text001');
+
+el001.addEventListener('click', function(){
+	console.log(`${this.attic}`);
+	var temp = this.getAttribute("attic");
 	this.attic = this.innerHTML;
-	this.innerHTML = "Drive My Car";
+	this.innerHTML = temp;
 
 })
 
-if (button.getAttribute("data-text-swap") == button.innerHTML) {
-    button.innerHTML = button.getAttribute("data-text-original");
-  } else {
-    button.setAttribute("data-text-original", button.innerHTML);
-    button.innerHTML = button.getAttribute("data-text-swap");
-  }
+
+
+// if (button.getAttribute("data-text-swap") == button.innerHTML) {
+//     button.innerHTML = button.getAttribute("data-text-original");
+//   } else {
+//     button.setAttribute("data-text-original", button.innerHTML);
+//     button.innerHTML = button.getAttribute("data-text-swap");
+//   }
