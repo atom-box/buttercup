2251fed691be3ac1c399998c9f8d6bafa78c288b
Evan Genest <genest.1@osu.edu>
2019-07-20 15:37:36 -0500

    Restore point.  Equals production copy, with MORE/LESS and mouseover on PROJECTS.

--- build/html/scripts/more.js
+++ build/html/scripts/more.js
@@ -21,9 +21,22 @@ el001.addEventListener('click', function(){
 	el001.setAttribute('attic', temp);
 	console.log(`What is in the attic after: ${this.getAttribute('attic')}`)
 
+// TODO rewrite this function:  (1) function just accepts THIS (2) event listener is an onclick in the HTML instead of in the JS
 
 }); // todo MAKE THIS 'THIS'
 
+var el002 = document.getElementById('text002');
+
+el002.addEventListener('click', function(){
+	console.log(`What is in the attic before: ${this.getAttribute('attic')}`)
+	var temp = el002.innerHTML;
+	el002.innerHTML = el002.getAttribute("attic");
+	el002.setAttribute('attic', temp);
+	console.log(`What is in the attic after: ${this.getAttribute('attic')}`)
+
+// TODO rewrite this function:  (1) function just accepts THIS (2) event listener is an onclick in the HTML instead of in the JS
+
+}); // todo MAKE THIS 'THIS'
 
 
 // if (button.getAttribute("data-text-swap") == button.innerHTML) {
